7ec9b8eb372f9bafc01c2d3cba703ebe621c1f71
e1b18034 <e1b18034@st.oit.ac.jp>
2020-12-02 16:05:51 +0900

    SamplePrefabの作成
    Prefabを使用しての360度写真切り替えを作成

---
+++ Assets/Scripts/UI/MoveButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButton : MonoBehaviour
+{
+    /* fields */
+    private LoadPrefab loadPrefab;
+
+    /* Start function */
+    void Start()
+    {
+        this.loadPrefab = new LoadPrefab();
+    }
+
+    /* Update function */
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // メインカメラ上のマウス位置からRayをとばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                string name = hit.collider.gameObject.name;
+                if(name.Equals("LeftButton"))
+                {
+                    this.loadPrefab.ChangePrefab(-1);
+                } else if(name.Equals("RightButton"))
+                {
+                    this.loadPrefab.ChangePrefab(1);
+                }
+            }
+        }
+    }
+}
